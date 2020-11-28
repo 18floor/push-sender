6956b06c7120d6850640ee0c2cd69e36df476cf4
Cface <cface@18floor.ru>
2020-11-28 19:02:11 +0300

    app postText

--- src/main/kotlin/ru/netology/pusher/main.kt
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -7,7 +7,6 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import java.io.FileInputStream
 
-
 fun main() {
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
@@ -20,9 +19,10 @@ fun main() {
         .putData("action", "LIKE")
         .putData("content", """{
           "userId": 1,
-          "userName": "Vasiliy",
+          "userName": "Геннадий",
           "postId": 2,
-          "postAuthor": "Netology"
+          "postAuthor": "Netology",
+          "postText": "Вам необходимо проанализировать, что произойдёт, если в приложение придёт Notification, у которого поле action не соответствует ни одному значению из Enum'а"
         }""".trimIndent())
         .setToken(token)
         .build()
